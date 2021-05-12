af7955099c8e1ce4137892f1330a69781834286b
Gyulai Dominik <detictum@gmail.com>
2021-05-12 20:17:51 +0200

    Added 'Instructions' button function and 'Quit game' button functions

--- src/main/java/hu/unideb/inf/boardgame/MainMenuController.java
+++ src/main/java/hu/unideb/inf/boardgame/MainMenuController.java
@@ -4,6 +4,7 @@ import hu.unideb.inf.boardgame.player.Player;
 import hu.unideb.inf.boardgame.player.PlayerService;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
 
@@ -28,12 +29,29 @@ public class MainMenuController extends Controller{
 
     @FXML
     void onInstructions(ActionEvent event) {
-
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Instructions");
+        alert.setHeaderText("Boardgame instructions");
+        alert.setContentText("""
+                This is a simple, chess-like boardgame
+                If you have not registered yet,
+                You need to, to play the game.
+                After that you can choose a side, Blue or Red.
+                
+                Rules of the game:
+                There are two restricted zones you can not step on.
+                You can only step forward and diagonally.
+                If you are willing to step diagonally, and there is
+                an enemy Disk on the chosen square, 
+                then the enemy's disk will be removed from the table.
+                The first player who can't make more moves loses.
+                """);
+        alert.showAndWait();
     }
 
     @FXML
     void onQuitGame(ActionEvent event) {
-
+        System.exit(0);
     }
 
     @FXML
